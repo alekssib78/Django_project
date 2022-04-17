62bc6cc7d158284046708586bf4f25852625535d
alekssib78 <alekssib78@gmal.com>
2022-04-17 19:08:49 +0300

    dz_1

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mainapp import views as mainapp
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', mainapp.index),
+    path('products', mainapp.products),
+    path('contact', mainapp.contact),
+
 ]
